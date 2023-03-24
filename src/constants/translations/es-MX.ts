83b3dbcf2c929093dc56526c52d6ae44a224c09e
gutierol <128817466+gutierol@users.noreply.github.com>
2023-03-24 09:33:20 -0400

    Update es-MX.ts

--- src/constants/translations/es-MX.ts
+++ src/constants/translations/es-MX.ts
@@ -4,58 +4,58 @@ import en_US from './en-US';
 const es_MX: Translation = {
   ...en_US,
   // DASHBOARD
-  CREDIT: 'Credito',
-  DEBIT: 'Debito',
-  RECENT_TRANSACTIONS: 'Transacciónes Recientes',
-  MY_ACCOUNTS: 'Mi Cuentas',
+  CREDIT: 'Crédito',
+  DEBIT: 'Débito',
+  RECENT_TRANSACTIONS: 'Transacciones Recientes',
+  MY_ACCOUNTS: 'Mis Cuentas',
 
   // ADD ACCOUNT
   ADD_ACCOUNT: 'Agregar Cuenta',
   ACCOUNT_NAME: 'Nombre de Cuenta',
-  INITIAL_AMOUNT: 'Cantidad Inicial',
+  INITIAL_AMOUNT: 'Monto Inicial',
   CREATE_ACCOUNT: 'Crear Cuenta',
 
   // EDIT ACCOUNT
   EDIT_ACCOUNT: 'Modificar Cuenta',
   UPDATE_ACCOUNT: 'Actualizar Cuenta',
   // ACCOUNT DETAILS
-  ACCOUNT_DETAILS: 'Detalles De Cuenta',
-  INITIAL_BALANCE: 'Cantidad Inicial',
-  DELETE_ACCOUNT: 'Borrar Cuenta?',
+  ACCOUNT_DETAILS: 'Detalles de Cuenta',
+  INITIAL_BALANCE: 'Balance Inicial',
+  DELETE_ACCOUNT: '¿Borrar Cuenta?',
   DELETE_ACCOUNT_INFO:
-    'Esto eliminara la cuenta {{accountName}} permanentemente, incluyendo {{transactionCount}} transacciónes conectadas.',
+    'Esto eliminara la cuenta {{accountName}} permanentemente, incluyendo {{transactionCount}} transacciones asociadas.',
 
   // NEW TRANSACTION
   NEW_TRANSACTION: 'Transacción Nueva',
   CREATE_TRANSACTION: 'Crear Transacción',
-  CATEGORY: 'Categoria',
-  SOURCE_ACCOUNT: 'Cuenta De Origen',
-  DESTINATION_ACCOUNT: 'Cuenta De Destino',
+  CATEGORY: 'Categoría',
+  SOURCE_ACCOUNT: 'Cuenta Origen',
+  DESTINATION_ACCOUNT: 'Cuenta Destino',
   TRANSFER: 'Transferir',
-  SHORT_DESCRIPTION: 'Description Corta',
-  AMOUNT: 'Cantidad',
+  SHORT_DESCRIPTION: 'Descripción Corta',
+  AMOUNT: 'Monto',
 
   // EDIT TRANSACTION
   EDIT_TRANSACTION: 'Modificar Transacción',
   UPDATE_TRANSACTION: 'Actualizar Transacción',
 
   // TRANSACTION DETAILS
-  TRANSACTION_DETAILS: 'Detalles De La Transacción',
+  TRANSACTION_DETAILS: 'Detalles de la Transacción',
   ACCOUNT: 'Cuenta',
   DATE_CREATED: 'Fecha Creada',
   DATE_UPDATED: 'Fecha Actualizada',
-  DELETE_TRANSACTION: 'Borrar Transacción?',
+  DELETE_TRANSACTION: '¿Borrar Transacción?',
   DELETE_TRANSACTION_INFO:
-    'Esto eliminara el record de transacción permanentemente.',
+    'Esto eliminara el registro de la transacción permanentemente.',
   KEEP: 'Guardar',
   DELETE: 'Borrar',
 
   // Transactions
-  TRANSACTIONS: 'Transacciónes',
+  TRANSACTIONS: 'Transacciones',
   SEARCH: 'Buscar',
-  DATE_RANGE: 'Periodo de Tiempo',
-  SHOW_ALL: 'Enseñar Todo',
-  EXPORT: 'Export',
+  DATE_RANGE: 'Período de Tiempo',
+  SHOW_ALL: 'Mostrar Todo',
+  EXPORT: 'Exportar',
 
   // SETTINGS
   SETTINGS: 'Ajustes',
